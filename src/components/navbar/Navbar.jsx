791c01be1ce7ab2d0de8e6fdc28eb5c74d8bb9d8
stwhello <sherinpaul2001@gmail.com>
2025-06-09 17:51:39 +0530

    initial commit

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+import Link from "next/link"
+
+const Navbar = () => {
+  return (
+      <div>
+          <div>Logo</div>
+          <div>
+              <Link href="/">HomePage</Link>
+              <Link href="/about">About</Link>
+              <Link href="/contact">Contact</Link>
+          </div>
+    </div>
+  )
+}
+
+export default Navbar
